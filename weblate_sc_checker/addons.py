48a395ede17918bde197e0d1ba480b5049172d44
Laeng <dkdi4236@gmail.com>
2024-05-05 23:23:48 +0900

    feat: add addon name and desc

--- weblate_sc_checker/addons.py
+++ weblate_sc_checker/addons.py
@@ -1,7 +1,9 @@
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy
 from weblate.addons.events import EVENT_PRE_COMMIT
 from weblate.addons.scripts import BaseScriptAddon
 
 
 class CheckerAddon(BaseScriptAddon):
     """Pre commit script example addon."""
+    name = gettext_lazy("StarCitizen Format check addon")
+    description = gettext_lazy("If you see this, you can use the Custom quality check for Star Citizen.")
